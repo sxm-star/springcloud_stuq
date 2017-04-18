e7e288f904bbb5a6242668b8c53b1a3a8e0ff31f
SongXuanMing <18720987043@139.com>
2017-04-19 02:57:49 +0800

    自由控制返回的json字段

--- demo-hello/src/main/java/cn/springboot/stuq/dto/DemoDTO.java
+++ demo-hello/src/main/java/cn/springboot/stuq/dto/DemoDTO.java
@@ -18,4 +18,7 @@ public class DemoDTO {
     @JSONField(format = "yyyy-MM-dd HH:mm")   //解决返回时直接返回日期
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")  //解决请求报错
     Date createTime;
+
+    @JSONField(deserialize = false)  //不想返回该字段信息,可以用这个注解的属性值
+    String remark;
 }

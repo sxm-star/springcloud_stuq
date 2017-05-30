cad8426bdf9f6887ca6cbaf25cf7448c1a5b4b0c
xuanmings <18720987043@139.com>
2017-05-31 00:36:00 +0800

    添加activiti5支持

---
+++ demo-activiti5/src/main/java/com/songxm/activiti5/DemoActiviti5Application.java
@@ -0,0 +1,12 @@
+package com.songxm.activiti5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActiviti5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoActiviti5Application.class, args);
+	}
+}

93011fc6378003de721dd83d384c8baf77bf4a4c
xuanmings <18720987043@139.com>
2017-05-30 16:33:38 +0800

    microservice-provider-user 已经注册到eureka中心了 localhost:8761可查看到目前注册上来的服务。

--- cloud-glance/microservice-provider-user/src/main/java/com/songxm/cloud/MicroserviceSimpleProviderUserApplication.java
+++ cloud-glance/microservice-provider-user/src/main/java/com/songxm/cloud/MicroserviceSimpleProviderUserApplication.java
@@ -2,8 +2,12 @@ package com.songxm.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //会员中心注册到eureka
+@EnableDiscoveryClient
 public class MicroserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {

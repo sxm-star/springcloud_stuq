6c77614a64446744a4ae7d4b3f612e07f692b543
xuanmings <18720987043@139.com>
2017-05-31 00:50:03 +0800

    添加测试用例自动生成25张表

--- demo-activiti5/src/test/java/com/songxm/activiti5/DemoActiviti5ApplicationTests.java
+++ demo-activiti5/src/test/java/com/songxm/activiti5/DemoActiviti5ApplicationTests.java
@@ -1,16 +1,35 @@
 package com.songxm.activiti5;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class DemoActiviti5ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    /**
+     * 测试生成activiti的25张表
+     */
+    @Test
+    public void testCreateTable() {
+        //获取流程引擎配置
+        ProcessEngineConfiguration pec = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+        pec.setJdbcDriver("com.mysql.jdbc.Driver"); //配置驱动
+        pec.setJdbcUrl("jdbc:mysql://localhost:3306/db_activiti"); //配置连接地址
+        pec.setJdbcUsername("root");
+        pec.setJdbcPassword("root");
+
+        //配置模式 true自动创建和更新表
+        pec.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        //获取流程引擎对象
+        ProcessEngine pe  =  pec.buildProcessEngine();
+    }
 
 }

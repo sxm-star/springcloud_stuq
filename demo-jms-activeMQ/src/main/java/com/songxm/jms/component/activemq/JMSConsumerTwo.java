09225d488f7819463f4fc14af5ca5a7878d4b3be
xuanmings <18720987043@139.com>
2017-05-30 20:32:17 +0800

    点对点 消费消息 第二种方式  监听模式推荐

---
+++ demo-jms-activeMQ/src/main/java/com/songxm/jms/component/activemq/JMSConsumerTwo.java
@@ -0,0 +1,56 @@
+package com.songxm.jms.component.activemq;
+
+import com.songxm.jms.component.listener.JMSListener;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.stereotype.Component;
+
+import javax.jms.*;
+
+/**
+ * @author: sxm
+ * @date: 16/11/30 19:51
+ * @version: v1.0.0
+ */
+@Component
+public class JMSConsumerTwo {
+    private static final String USERNAME= ActiveMQConnection.DEFAULT_USER;
+    private static final String PASSWD = ActiveMQConnection.DEFAULT_PASSWORD;
+    private static final String BROKEURL= ActiveMQConnection.DEFAULT_BROKER_URL;
+    private static final int SENDNUM = 10;
+
+
+    public static void main(String[] args) {
+
+        ConnectionFactory connectionFactory;//连接工厂
+        Connection connection = null;//连接
+        Session session;//发送或者接受消息的线程
+        Destination destination; //消息的目的地
+        MessageConsumer messageConsumer; //消息消费者
+        connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWD, BROKEURL);
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);//创建session,TRUE标志位事务
+            destination = session.createQueue("First-Queue"); //创建消息队列
+            messageConsumer = session.createConsumer(destination); //创建消息生产者
+
+           messageConsumer.setMessageListener(new JMSListener()); //注册消息监听
+
+            while (true){
+
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}

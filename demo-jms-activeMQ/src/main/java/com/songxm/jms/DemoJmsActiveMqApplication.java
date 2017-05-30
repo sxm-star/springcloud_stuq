8599dc84872334fd07de03c801e72c8d9c677449
xuanmings <18720987043@139.com>
2017-05-30 20:07:34 +0800

    activeMQ 点对点模型

---
+++ demo-jms-activeMQ/src/main/java/com/songxm/jms/DemoJmsActiveMqApplication.java
@@ -0,0 +1,12 @@
+package com.songxm.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJmsActiveMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJmsActiveMqApplication.class, args);
+	}
+}
